2f18fcf013c9b9fbcff813959f020594daeaf672
Peter Klein <pmk@io.dk>
2019-02-05 23:02:33 +0100

    cleanup

--- dist/examples/javascript/cleave.js
+++ dist/examples/javascript/cleave.js
@@ -1,6 +1,18 @@
+// Cleave onCreditCardTypeChanged handler
 var creditCardTypeChangedHandler = function(type) {
   var el = this.element.nextSibling;
   if (el && el.classList.contains('help-block')) {
     el.innerHTML = 'Card type: <strong>' + type + '</strong>';
   }
-}
+};
+
+// Save current Schema and Startval
+var schema = jsoneditor.schema;
+var startval = jsoneditor.getValue();
+
+// Patch schema options and add Cleave onCreditCardTypeChanged function
+schema.properties.creditcard.options.cleave.onCreditCardTypeChanged = creditCardTypeChangedHandler;
+
+// Regenerate the form
+if (jsoneditor) jsoneditor.destroy();
+jsoneditor = new window.JSONEditor(document.querySelector("#json-editor-form"),{schema: schema, startval: startval});
