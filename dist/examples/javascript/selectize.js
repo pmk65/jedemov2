16783a21f02788c525cb8aaf88e61bd912e86603
Peter Klein <pmk@io.dk>
2019-02-06 14:57:05 +0100

    custom comments

--- dist/examples/javascript/selectize.js
+++ dist/examples/javascript/selectize.js
@@ -1,1 +1,10 @@
+// Save current Schema and Startval
+var schema = jsoneditor.schema;
+var startval = jsoneditor.getValue();
+
+// Enable Selectize
 JSONEditor.plugins.selectize.enable = true;
+
+// Regenerate the form
+if (jsoneditor) jsoneditor.destroy();
+jsoneditor = new window.JSONEditor(document.querySelector("#json-editor-form"),{schema: schema, startval: startval});
