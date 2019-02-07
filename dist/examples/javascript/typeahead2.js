8f5a168580c8fde79b3f07651996e39196a5a751
Peter Klein <pmk@io.dk>
2019-02-07 13:42:06 +0100

    more examples

---
+++ dist/examples/javascript/typeahead2.js
@@ -0,0 +1,39 @@
+// includeJS("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js")
+// includeJS("https://cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.bundle.min.js")
+// includeCSS("https://pmk65.github.io/jedemov2/dist/examples/bloodhound.css")
+
+// Initialize the Typeahead/Bloodhound engine
+// (See Typeahead/Bloodhound documentation for more info)
+var zipEngine = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace("navn"),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: {
+    url: "https://dawa.aws.dk/postnumre/%QUERY",
+    wildcard: "%QUERY",
+    cache: true
+  }
+});
+zipEngine.clearPrefetchCache();
+zipEngine.clearRemoteCache();
+zipEngine.initialize();
+
+// When JSON-Editor is ready perform action on field(s)
+jseditor.on('ready', function() {
+
+  // Watch for changes in the "zip" field
+  jseditor.watch('root.zip',function() {
+    // Get value from "zip" field
+    var zipValue = jseditor.getEditor('root.zip').getValue();
+    // If zipvalue a 4 digit number?
+    if (/\d{4}/.test(zipValue)) {
+      // Do an AJAX lookup using Bloodhound engine
+      zipEngine.search(zipValue, null, function(res) {
+        if (res.length) {
+          // Set the value of "city" field to the returned value
+          jseditor.getEditor('root.city').setValue(res[2]);
+        }
+      });
+    }
+  });
+
+});
