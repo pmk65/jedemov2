d3a1cf3fb95a22f82e89d8f4e8e738f94088a9f0
Peter Klein <pmk@io.dk>
2019-02-24 14:47:59 +0100

    Examples cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function(grunt) {
     'string-replace': {
       version: {
         files: {
-          'src/': ['src/css/*.css', 'src/js/*.js'],
           'dist/': ['dist/css/*.css', 'dist/js/*.js']
         },
         options: {
